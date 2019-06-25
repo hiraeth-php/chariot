efb30724128918c6b923c9a4bbfaec50db6484de
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-06-25 01:23:54 -0700

    Offload more work to routing

--- src/proxies/RouterProxy.php
+++ src/proxies/RouterProxy.php
@@ -3,6 +3,7 @@
 namespace Hiraeth\Chariot;
 
 use Hiraeth;
+use Hiraeth\Routing\Route;
 use Awesomite\Chariot\Pattern\PatternRouter;
 use Awesomite\Chariot\Exceptions\HttpException;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -31,29 +32,34 @@ class RouterProxy implements Hiraeth\Routing\RouterInterface
 	 * @access public
 	 * @param Request $request The server request to try and match against a route
 	 * @param Response $response The default response to be modified in the event of errors
-	 * @return mixed A matching target for the route
+	 * @return Route The route to run
 	 */
-	public function match(Request $request, Response $response)
+	public function match(Request $request, Response $response): Route
 	{
+		$params = array();
+
 		try {
-			return $this->router->match($request->getMethod(), $request->getURI()->getPath());
+			$route  = $this->router->match($request->getMethod(), $request->getURI()->getPath());
+			$target = $route->getHandler();
+			$params = $route->getParams();
 
 		} catch(HttpException $e) {
 
 			switch ($e->getCode()) {
 				case HttpException::HTTP_NOT_FOUND:
-					return $response->withStatus(404);
+					$target = $response->withStatus(404);
+					break;
 
 				case HttpException::HTTP_METHOD_NOT_ALLOWED:
 					$allowed = $this->router->getAllowedMethods($request->getURI()->getPath());
-
-					return $response
-						->withStatus(405)
-						->withHeader('Allow', implode(', ', $allowed));
+					$target  = $response->withStatus(405)->withHeader('Allow', implode(', ', $allowed));
+					break;
 
 				default:
 					throw $e;
 			}
 		}
+
+		return new Route($target, $params);
 	}
 }
